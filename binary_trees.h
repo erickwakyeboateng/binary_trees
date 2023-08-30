6e90402d5bb279636d6b63d3825c5216118570a2
eric <boateng.eric44@gmail.com>
2023-08-30 17:28:00 +0000

    binary

--- binary_trees.h
+++ binary_trees.h
@@ -50,6 +50,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 
 #endif /* _BINARY_TREES_H_ */
