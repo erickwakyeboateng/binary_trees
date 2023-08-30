b45c4ed1a14bad3f0e4f719b5ba1482dd9a3b5e8
eric <boateng.eric44@gmail.com>
2023-08-30 16:54:54 +0000

    binary

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int le_ft = 0, ri_ght = 0;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
 	{
@@ -15,17 +15,17 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree->le_ft && tree->ri_ght)
+		if (tree->left && tree->right)
 		{
-			le_ft = binary_tree_is_full(tree->le_ft);
-			ri_ght = binary_tree_is_full(tree->ri_ght);
-			if (le_ft == 0 || ri_ght == 0)
+			left = binary_tree_is_full(tree->left);
+			right = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
 			{
 				return (0);
 			}
 			return (1);
 		}
-		else if (!tree->le_ft && !tree->ri_ght)
+		else if (!tree->left && !tree->right)
 		{
 			return (1);
 		}
